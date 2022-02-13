aeeecb9751868b4503bc466b72aebe9decc9c504
Jake <prevou15@gmail.com>
2022-02-13 10:53:58 -0600

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+
+function Keypad() {
+    function handleChange(event) {
+        console.log("Entering password...");
+        console.log(event)
+    }
+    return (
+        <input type="password" onChange={handleChange}/>
+    )
+}
+
+
+export default Keypad;
